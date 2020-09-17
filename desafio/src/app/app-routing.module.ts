33b724e8e010b317f467cb675c6889e56ce6a0d8
pedroleaop <pedroleaop@hotmail.com>
2020-09-17 19:47:54 -0300

    rotas adicionadas

--- desafio/src/app/app-routing.module.ts
+++ desafio/src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BrennandComponent } from './components/brennand/brennand.component';
+import { CulinariaComponent } from './components/culinaria/culinaria.component';
+import { FormComponent } from './components/form/form.component';
+import { HomemDoNordesteComponent } from './components/homem-do-nordeste/homem-do-nordeste.component';
+import { LocaisComponent } from './components/locais/locais.component';
+import { LocalComponent } from './components/local/local.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { StartPageComponent } from './components/start-page/start-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'',
+  component: NavigationComponent,
+  children: [
+    {path:'',
+    component: StartPageComponent},
+    {path:'local',
+    component: LocalComponent},
+    {path:'locais',
+    component: LocaisComponent},
+    {path:'brennand',
+    component: BrennandComponent},
+    {path:'homem-do-nordeste',
+    component: HomemDoNordesteComponent},
+    {path:'contato',
+    component: FormComponent}
+  ]},
+
+  {path:'culinaria',
+  component:CulinariaComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
