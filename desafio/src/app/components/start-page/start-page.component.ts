913831f85665a3d396967b67c1697cc42f1d8132
mgabrielaaf <mgabrielaaurelianof@gmail.com>
2020-09-17 22:15:24 -0300

    ultimo

--- desafio/src/app/components/start-page/start-page.component.ts
+++ desafio/src/app/components/start-page/start-page.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartPageComponent implements OnInit {
 
+  brennand:string = 'https://imagens1.ne10.uol.com.br/blogsne10/social1/uploads/2017/05/oficinabrennand.jpg';
+  museu:string = ' https://upload.wikimedia.org/wikipedia/commons/f/f8/Fachada_do_Muhne.jpg';
+  marco:string= 'https://media-cdn.tripadvisor.com/media/photo-s/18/00/2e/31/recife.jpg';
+
   constructor() { }
 
   ngOnInit(): void {
